ba3cf443c23e5b5ca17ed223b151434b3e40dee6
CrystalxBlade <satish141m@gmail.com>
2025-11-25 21:57:30 +0530

    destroy

--- Assets/Scripts/MovePipe.cs
+++ Assets/Scripts/MovePipe.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class MovePipe : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
+
+    void Start() 
+    {
+        Destroy(gameObject, 4f);
+    }
     void Update()
     {
         transform.position += Vector3.left * moveSpeed * Time.deltaTime; 
-        Destroy(gameObject, 4f);
     }
 }
