af0c5c7fdf7a29934e84d6972a8a4ebf2f6bc0f5
CrystalxBlade <satish141m@gmail.com>
2025-11-28 12:55:40 +0530

    new

--- Assets/Scripts/Flappy.cs
+++ Assets/Scripts/Flappy.cs
@@ -2,18 +2,31 @@ using UnityEngine;
 
 public class Flappy : MonoBehaviour
 {
+    [SerializeField] bool isDead = false;
+    Animator anim;
+    Collider2D coll;
     [SerializeField] float speed;
     [SerializeField] Rigidbody2D rb;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
     }
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0) && isDead == false)
         {
             rb.linearVelocity = Vector2.up * speed;
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        isDead = true;
+        anim.Play("die");
+        coll.enabled = false;
+        rb.linearVelocity = Vector2.up * speed;
     }
 }
