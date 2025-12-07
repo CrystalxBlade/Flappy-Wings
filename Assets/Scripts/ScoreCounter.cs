edae7837d4edc9feaef582401d23b5d10c5d880e
CrystalxBlade <satish141m@gmail.com>
2025-12-07 18:23:39 +0530

    score

---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+    public Text scoreText;
+    public int score;
+    void Start()
+    {
+        score = 0;
+    }
+
+    void Update()
+    {
+        
+    }
+}
