5d767449d72b341657767e72cd7fbffdab56f724
CrystalxBlade <satish141m@gmail.com>
2025-12-13 16:14:13 +0530

    new

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -5,5 +5,16 @@ public class SoundManager : MonoBehaviour
     public AudioSource Sound_SFX;
     public AudioClip flap, hit, point;
 
-    
+    public void Play_Flap()
+    {
+        Sound_SFX.PlayOneShot(flap);
+    }
+    public void Play_Hit()
+    {
+        Sound_SFX.PlayOneShot(hit);
+    }
+    public void Play_Point()
+    {
+        Sound_SFX.PlayOneShot(point);
+    }
 }
