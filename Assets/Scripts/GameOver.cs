3839c22d6086d5d46160b40e2d6fa15cb5570912
CrystalxBlade <satish141m@gmail.com>
2025-12-13 16:11:57 +0530

    new

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -23,7 +23,6 @@ public class GameOver : MonoBehaviour
 
         scoreText.text = score.ToString();
     }
-
     public void Retry()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource Sound_SFX;
+    public AudioClip flap, hit, point;
+
+    
+}
