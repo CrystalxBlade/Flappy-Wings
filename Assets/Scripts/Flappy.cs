1a2720394404aca7c20ea87f34f45ecf49794b4e
CrystalxBlade <satish141m@gmail.com>
2026-06-15 18:14:37 +0530

    movePipe

--- Assets/Scripts/Flappy.cs
+++ Assets/Scripts/Flappy.cs
@@ -2,12 +2,18 @@ using UnityEngine;
 
 public class Flappy : MonoBehaviour
 {
+    [SerializeField] float speed;
+    Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-        
+        if(Input.GetMouseButton(0))
+        {
+            rb.linearVelocity = Vector2.up * speed;
+        }
     }
+
 }
